db219128ecd54e5d69eb50e8e56cee271dd4a6b5
BEY Suchea <chang.ouchi01@gmail.com>
2022-08-24 06:29:35 +0700

    Add files via upload

---
+++ includes/errors/403.php
@@ -0,0 +1,96 @@
+<?php
+ require_once 'core/init.php'; 
+ 
+$user = new User();
+
+if(!$user->isLoggedIn()) {
+    Redirect::to('login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- META SECTION -->
+        <title>COERR</title>            
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <!-- END META SECTION -->
+        
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="includes/css/theme-default.css"/>
+        <!-- EOF CSS INCLUDE -->
+    
+    </head>
+    <body>
+
+        <!-- START PAGE CONTAINER -->
+        <div class="page-container">
+            
+            <?php $parent_page = ""; ?>
+            
+            <!-- START PAGE SIDEBAR -->
+            <?php include 'includes/sidebar.php'; ?>
+            <!-- END PAGE SIDEBAR -->
+            
+            <!-- PAGE CONTENT -->
+            <div class="page-content">
+                    
+                <!-- START X-NAVIGATION VERTICAL -->
+                <?php include 'includes/x_navigation.php'; ?>
+                <!-- END X-NAVIGATION VERTICAL -->                     
+
+                <!-- START BREADCRUMB -->
+                <ul class="breadcrumb">
+                    <li><a href="#">Home</a></li>                          
+                    <li class="active">log</li>
+                </ul>
+                <!-- END BREADCRUMB -->                       
+                
+                <!-- PAGE CONTENT WRAPPER -->
+                <div class="page-content-wrap" id="page-content-wrap">
+                    
+                     <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="error-container">
+                                <div class="error-code">403</div>
+                                <div class="error-text">Access Denied</div>
+                                <div class="error-subtext">Unfortunately we're having trouble loading the page you are looking for. Please contact administrator or use action below.</div>
+                                <div class="error-actions">                                
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <button class="btn btn-info btn-block btn-lg" onClick="document.location.href = 'index.php';">Back to dashboard</button>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <a href="#" class="btn btn-primary btn-block btn-lg">Previous page</a>
+                                        </div>
+                                    </div>                                
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                                     
+                </div>
+                <!-- END PAGE CONTENT WRAPPER -->                                
+            </div>            
+            <!-- END PAGE CONTENT -->
+        </div>
+        <!-- END PAGE CONTAINER -->
+
+
+        <!-- MESSAGE BOX-->
+            <?php require 'includes/messagebox.php'; ?>
+        <!-- END MESSAGE BOX-->
+        
+        <?php require 'includes/footer.php'; ?>
+
+    </body>
+</html>
+
---
+++ includes/errors/404.php
@@ -0,0 +1,106 @@
+<?php
+ require_once 'core/init.php'; 
+ 
+$user = new User();
+
+if(!$user->isLoggedIn()) {
+    Redirect::to('login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- META SECTION -->
+        <title>COERR</title>            
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <!-- END META SECTION -->
+        
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="includes/css/theme-default.css"/>
+        <!-- EOF CSS INCLUDE -->
+    
+    </head>
+    <body>
+
+        <!-- START PAGE CONTAINER -->
+        <div class="page-container">
+            
+            <?php $parent_page = ""; ?>
+            
+            <!-- START PAGE SIDEBAR -->
+            <?php include 'includes/sidebar.php'; ?>
+            <!-- END PAGE SIDEBAR -->
+            
+            <!-- PAGE CONTENT -->
+            <div class="page-content">
+                    
+                <!-- START X-NAVIGATION VERTICAL -->
+                <?php include 'includes/x_navigation.php'; ?>
+                <!-- END X-NAVIGATION VERTICAL -->                     
+
+                <!-- START BREADCRUMB -->
+                <ul class="breadcrumb">
+                    <li><a href="#">Home</a></li>                          
+                    <li class="active">log</li>
+                </ul>
+                <!-- END BREADCRUMB -->                       
+                
+                <!-- PAGE CONTENT WRAPPER -->
+                <div class="page-content-wrap" id="page-content-wrap">
+                    
+                     <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="error-container">
+                                <div class="error-code">404</div>
+                                <div class="error-text">page not found</div>
+                                <div class="error-subtext">Unfortunately we're having trouble loading the page you are looking for. Please wait a moment and try again or use action below.</div>
+                                <div class="error-actions">                                
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <button class="btn btn-info btn-block btn-lg" onClick="document.location.href = 'index.html';">Back to dashboard</button>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <button class="btn btn-primary btn-block btn-lg" onClick="history.back();">Previous page</button>
+                                        </div>
+                                    </div>                                
+                                </div>
+                                <div class="error-subtext">Or you can use search to find anything you need.</div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="input-group">
+                                            <input type="text" placeholder="Search..." class="form-control"/>
+                                            <div class="input-group-btn">
+                                                <button class="btn btn-primary"><span class="fa fa-search"></span></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                                     
+                </div>
+                <!-- END PAGE CONTENT WRAPPER -->                                
+            </div>            
+            <!-- END PAGE CONTENT -->
+        </div>
+        <!-- END PAGE CONTAINER -->
+
+
+        <!-- MESSAGE BOX-->
+            <?php require 'includes/messagebox.php'; ?>
+        <!-- END MESSAGE BOX-->
+        
+        <?php require 'includes/footer.php'; ?>
+
+    </body>
+</html>
+
